bb2210b8566bf40ef4658c6f1a244606bc3f2dcc
David Lawrence <dkl@mozilla.com>
2024-09-24 17:23:50 -0400

    Add project name mapping

--- todoist.ts
+++ todoist.ts
@@ -5,6 +5,24 @@ import { TodoistApi } from 'todoist';
 import { readSecrets } from "$sb/lib/secrets_page.ts";
 import { editor } from "$sb/syscalls.ts";
 
+const projectIdMap = {};
+
+async function getProjectIdMap(): Promise<object> {
+  if (Object.keys(projectIdMap).length) {
+    return projectIdMap;
+  }
+
+  const [token] = await readSecrets(["todoistToken"]);
+  const api = await new TodoistApi(token);
+
+  const projectList = await api.getProjects();
+
+  for (let i = 0; i < projectList.length; i++) {
+    projectIdMap[projectList[i].id] = projectList[i].name;
+  }
+
+  return projectIdMap;
+}
 
 export async function getTasks({ query }: QueryProviderEvent): Promise<any[]> {
   const [token] = await readSecrets(["todoistToken"]);
@@ -20,6 +38,17 @@ export async function getTasks({ query }: QueryProviderEvent): Promise<any[]> {
 
   const tasks = await api.getTasks({filter: filter});
 
+  const projectMap = await getProjectIdMap();
+
+  for (let i = 0; i < tasks.length; i++) {
+    if (projectMap[tasks[i].projectId]) {
+      tasks[i].projectName = projectMap[tasks[i].projectId];
+    }
+    else {
+      tasks[i].projectName = "None";
+    }
+  }
+
   const result = applyQuery(
     query,
     tasks.map((p) => flattenObject(p)),
@@ -33,6 +62,7 @@ export async function addInboxTask(pageName: string, text: string) {
   const api = await new TodoistApi(token);
 
   const task = await editor.prompt("Add to Todoist Inbox:", "");
+
   if (!task) {
     return;
   }
